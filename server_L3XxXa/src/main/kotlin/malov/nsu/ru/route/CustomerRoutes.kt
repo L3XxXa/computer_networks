ab2c5580d77de6c8622e86d53b227eb32837a76a
L3XxXa <alexkkk@inbox.ru>
2022-04-19 00:21:48 +0700

    Some reworks

--- server_L3XxXa/src/main/kotlin/malov/nsu/ru/route/CustomerRoutes.kt
+++ server_L3XxXa/src/main/kotlin/malov/nsu/ru/route/CustomerRoutes.kt
@@ -3,17 +3,24 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.util.Date
 
 
-fun Route.customerRouting(){
-    route("/check"){
+fun Route.customerRouting() {
+    route("/check") {
         get {
             call.respondText("I AM WORKING", status = HttpStatusCode.OK)
         }
     }
-    route("/sayhello"){
+    route("/sayhello") {
         get {
             call.respondText("I don't want to talk with you", status = HttpStatusCode.OK)
+        }
+    }
+
+    route("/responsehtml") {
+        get {
+            call.respondText("Hello with HTML", ContentType.Text.Html, status = HttpStatusCode.OK)
         }
     }
 }
